fd10f162aaa89d7cdb252725054e2a209e4f4cb1
Ema Stefanikova <x467942@fi.muni.cz>
2021-06-17 15:37:52 +0200

    Iteration 06 final

--- messenger-api/prisma/seed.ts
+++ messenger-api/prisma/seed.ts
@@ -8,7 +8,7 @@ async function main() {
     update: {},
     create: {
       firstName: `Anna`,
-      lastName: 'Black',
+      lastName: 'Black'
     },
   })
 
@@ -18,6 +18,123 @@ async function main() {
     update: {},
     create: {
       firstName: `Alex`,
-      lastName: 'Cooper',
-    },
-  })}
+      lastName: 'Cooper'
+    }
+  })
+
+const message1 = await prisma.message.upsert({
+  where: { 
+    id: 1 },
+update: {},
+create: {
+  senderId:1,
+  content: "Hello",
+  conversationId:1
+}
+})
+
+const message2 = await prisma.message.upsert({
+  where: { 
+    id: 2 },
+update: {},
+create: {
+  senderId:1,
+  content: "Hello",
+  conversationId:1
+}
+})
+
+const message3 = await prisma.message.upsert({
+  where: { 
+    id: 3 },
+update: {},
+create: {
+  senderId:1,
+  content: "Hello",
+  conversationId:1
+}
+})
+
+const message4 = await prisma.message.upsert({
+  where: { 
+    id: 4 },
+update: {},
+create: {
+  senderId:1,
+  content: "Hello",
+  conversationId:1
+}
+})
+
+const message5 = await prisma.message.upsert({
+  where: { 
+    id: 5 },
+update: {
+  content:"???"
+},
+create: {
+  senderId:1,
+  content: "How are you",
+  conversationId:1
+}
+})
+
+const message6 = await prisma.message.upsert({
+  where: { 
+    id: 6 },
+update: {},
+create: {
+  senderId:2,
+  content: "Calm down",
+  conversationId:1
+}
+})
+
+const message7 = await prisma.message.upsert({
+  where: { 
+    id: 7 },
+update: {},
+create: {
+  senderId:1,
+  content: "Calm down",
+  conversationId:1
+}
+})
+
+const conversation1 = await prisma.conversation.upsert({
+  where: { 
+    id: 1 },
+update: {},
+create: {
+}
+})
+
+const conversationUser1 = await prisma.conversationUser.upsert({
+  where: { 
+    id: 1 },
+update: {},
+create: {
+  conversationId: 1,
+  userId: 1
+}
+})
+
+const conversationUser2 = await prisma.conversationUser.upsert({
+  where: { 
+    id: 2 },
+update: {},
+create: {
+  conversationId: 1,
+  userId: 2
+}
+})
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+}
--- messenger-api/src/index.ts
+++ messenger-api/src/index.ts
@@ -14,13 +14,17 @@ async function main() {
   //await prisma.message.create({ data:message2  });
   //await prisma.message.update({ where: { id: 6 }, data: { content: "calm down"}, })
   //const dates = allMessages.map(d => d.createdAt);
-  //const allusers = await prisma.user.findMany();
+  //const allusers = await prisma.conversationUser.findMany();
+  //console.log(allusers);
 
               //--Used to create data finish--
     
     const messagesFromRightUser = await prisma.message.findMany({
       where: {
-        senderId: 1}});
+        senderId: 1
+      }
+    }
+    );
     const messageLastId = Math.max(...messagesFromRightUser.map(user => user.id));
     const lastMessage = messagesFromRightUser.filter(m=> m.id==messageLastId);
     const textLastMessage = lastMessage.map(m => m.content);
